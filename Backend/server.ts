64700b72b5a0635132e76f9fe0fd5bc200a282f8
Rachna Yadav <rachna20032004@gmail.com>
2026-04-06 00:50:59 +0530

    third commit

--- Backend/server.ts
+++ Backend/server.ts
@@ -15,7 +15,10 @@ async function startServer() {
   const app = express();
   const PORT = process.env.PORT || 3000;
 
-  app.use(cors());
+  app.use(cors({
+    origin:"*"
+  }));
+  
   app.use(morgan("dev"));
   app.use(express.json());
 
